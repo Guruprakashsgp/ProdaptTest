f4d8e0bf9337e98f591538ef38cba7a10b80aa24
Guruprakash S <guruprakash.s@prodapt.com>
2023-07-31 16:54:05 +0530

    my first commit

---
+++ test.java
@@ -0,0 +1,108 @@
+import java.util.*;
+public class test {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter the distance:(between 5 kn to 8 km)");
+//		int km=sc.nextInt();
+//		if(km <5)
+//		{
+//			System.out.println("Sorry, Choose between 5km to 8km");
+//		}
+//		
+//		for(int i=(km-1);i>0;i--)
+//			{
+//				System.out.println("Distance to run : "+ i);
+//				if(i==km-1)
+//				{
+//					System.out.println("Good start Keep it up");
+//				}
+//				if(i<3)
+//				{
+//					System.out.println("Almost there");
+//				}
+//				if(i==1)
+//				{
+//					System.out.println("Done for the day");
+//				}
+//			}
+		
+////////////////////////////////////////////////////////////////////////////
+//		    System.out.println("Enter the profit and loss values");
+//			int n=sc.nextInt();
+//			int[] a=new int[n];
+//			ArrayList<Integer> arr=new ArrayList<Integer>();
+//			for (int i=0;i<n;i++)
+//			{
+//				a[i]=sc.nextInt();
+//			}
+//			for (int i=0;i<n-1;i++)
+//			{
+//				int sum=0;
+//				for(int j=i+1;j<n;j++)
+//				{
+//					sum=sum+a[j];
+//					arr.add(sum);
+//				}
+//				
+//			}
+//			System.out.println(Collections.max(arr));
+//			
+///////////////////////////////////////////////////////////////////////////
+		    
+		
+//			System.out.println("Enter the tresure number");
+//		    int n=sc.nextInt();
+//		    int sum=0;
+//            ArrayList<Integer> al=new ArrayList<Integer>();
+//            for(int i=0;i<n;i++)
+//            {
+//            	al.add(sc.nextInt());
+//            }
+//            for (int i=0;i<n;i++)
+//            {
+//            	sum=sum+al.get(i);
+//            }
+//            if(sum%2==0)
+//            {
+//            	System.out.println("True");
+//            }
+//            else
+//            {
+//            	System.out.println("False");
+//            }
+		
+////////////////////////////////////////////////////////////////////////////
+//	    System.out.println("Enter the number of platforms");
+//	    int n=sc.nextInt();
+//		int[] arr= new int[n];
+//		int jump=0;
+//		int jumpval=0;
+//		for(int i=0;i<=n;i++)
+//		{
+//			arr[i]=sc.nextInt();
+//		}
+//		for (int i=0;i<=arr.length;i++)
+//		{
+//		   if(arr[0]>0)
+//		   {
+//			   
+//		   }
+//		}
+		
+/////////////////////////////////////////////////////////////////////////////
+		
+//		System.out.println("Enter the number of students");
+//		int n=sc.nextInt();
+//		ArrayList<String> al= new ArrayList<String>();
+//		for (int i=0;i<=n;i++)
+//		{
+//			al.add(sc.next());
+//		}
+//		Collections.sort(al);
+//		System.out.println(al);
+		
+////////////////////////////////////////////////////////////////////////////		
+	}
+
+}
